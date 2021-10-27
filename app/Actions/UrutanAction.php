1f46e7665d2aea698281d12dceb8c4c59b1047d3
amrdevac <amrullahdev@yahoo.com>
2021-10-27 16:42:50 +0700

    menambahkan urutan action untuk mencari urutan tersedia pada sebuah data

---
+++ app/Actions/UrutanAction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Actions;
+
+class UrutanAction
+{
+    public function resufleUrutanIfExist($table, $nama_kolom, $old, $new)
+    {
+        DB::table($table)->where($nama_kolom, $new)->update(
+            ["urutan_sidebar" => $old]
+        );
+        return $new;
+    }
+
+    public function getUrutanSidebar($nama_table, $nama_kolom)
+    {
+        $urutan = DB::table($nama_table)->max($nama_kolom);
+        return  $urutan + 1;
+    }
+}
