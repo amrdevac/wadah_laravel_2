df278d3c0b3a3d5d883a817350eb539e462e8e0a
amrdevac <amrullahdev@yahoo.com>
2021-10-29 09:28:52 +0700

    fixing base url untuk mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require("laravel-mix");
  */
 
 mix.browserSync({
-    proxy: "http://127.0.0.1:8000"
+    proxy: "http://127.0.0.1:8001"
 });
 mix.js("resources/js/app.js", "public/js")
     .vue()
