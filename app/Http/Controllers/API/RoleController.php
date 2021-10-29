b0e4c26fb4ccb0ae6fbfe3d1ebea80f6de25d016
amrdevac <amrullahdev@yahoo.com>
2021-10-29 09:28:04 +0700

    implementasi service pada role controller

--- app/Http/Controllers/API/RoleController.php
+++ app/Http/Controllers/API/RoleController.php
@@ -3,13 +3,48 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Services\ResponseService;
+use App\Services\RoleService;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
+    private RoleService $roleService;
+    private ResponseService $response;
+
+    public function __construct(RoleService $roleService, ResponseService $response)
+    {
+        $this->roleService = $roleService;
+        $this->response = $response;
+    }
+
     public function index()
     {
-        $in_data = ["123", 'asdf', "sadfasdf"];
-        return compact("in_data");
+
+        $in_role = $this->roleService->mendapatkanSeluruhData();
+        return compact("in_role");
+    }
+
+    public function show($id)
+    {
+        $in_role = $this->roleService->mendapatkanSeluruhData();
+        return compact("in_role");
+    }
+
+    public function store(Request $request)
+    {
+        return $this->response->menyimpanData($this->roleService->menyimpanData($request));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->roleService->memperbaruiDatamendapatkanSeluruhData($request, $id);
+        return $this->response->updateData($request->all());
+    }
+
+    public function destroy($id)
+    {
+        $this->roleService->menghapusDatamendapatkanSeluruhData($id);
+        return $this->response->menghapusData($id);
     }
 }
