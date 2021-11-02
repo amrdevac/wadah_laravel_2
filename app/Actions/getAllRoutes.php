3b2107948f4985f8f8a159f4a0f6c69eeb3dd70d
amrdevac <amrullahdev@yahoo.com>
2021-11-02 16:13:20 +0700

    fixing eimplementasi getAllRoutes action

--- app/Actions/getAllRoutes.php
+++ app/Actions/getAllRoutes.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Action;
+namespace App\Actions;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 class getAllRoutes
@@ -9,31 +10,40 @@ class getAllRoutes
 
     public function set_default_route_name_list()
     {
-        $data = Route::getRoutes('attributes');
+        $data_url = Route::getRoutes('attributes');
         $rename_label = [
             'index' => 'Daftar',
-            'create' => 'Mengelola',
+            'create' => 'Menambah',
             'store' => 'Menyimpan',
             'show' => 'Detail',
             'update' => 'Update',
+            'edit' => 'Mengubah',
             'destroy' => 'Menghapus',
+            'search' => 'Pencarian',
         ];
-        foreach ($data as  $value) {
-            if (explode('/', $value->uri)[0] == 'api') {
-                if (empty($value->action['as'])) {
-                    dd($value);
+        
+        foreach ($data_url as  $url) {
+            if (explode('/', $url->uri)[0] == 'api') {
+                if (empty($url->action['as'])) {
+                    dd($url->action);
                 }
-                if (count(explode('.', $value->action['as'])) > 1) {
-                    $cek_duplikasi_data_api = DB::table('permission')
-                        ->where('route_url', $value->action['as'])
+
+                $mendapatkan_jenis = explode('.', $url->action['as'])[1];
+                if ($mendapatkan_jenis  == "edit" || $mendapatkan_jenis == "create") {
+                     continue;
+                }
+                
+                if (count(explode('.', $url->action['as'])) > 1) {
+                    $cek_duplikasi_data_api = DB::table('permissions')
+                        ->where('route_url', $url->action['as'])
                         ->first();
 
-                    $data_explode = explode('.', $value->action['as']);
+                    $data_explode = explode('.', $url->action['as']);
                     if (!$cek_duplikasi_data_api) {
                         $data_nama = $rename_label[$data_explode[1]] ?? $data_explode[1];
-                        DB::table('permission')->insert([
+                        DB::table('permissions')->insert([
                             'nama_route' => $data_nama  . ' ' . $data_explode[0],
-                            'route_url' => $value->action['as'],
+                            'route_url' => $url->action['as'],
                             'nama_grup' => $data_explode[0],
                         ]);
                     }
