6030b18c9be0dbfff996ff0d61dcc1add55e7032
amrdevac <amrullahdev@yahoo.com>
2021-11-02 16:19:22 +0700

    kelola user modul

--- app/Http/Controllers/API/KelolaUserController.php
+++ app/Http/Controllers/API/KelolaUserController.php
@@ -3,9 +3,71 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Services\KelolaUserService;
+use App\Services\ResponseService;
+use App\Services\RoleService;
 use Illuminate\Http\Request;
 
 class KelolaUserController extends Controller
 {
-    //
+
+    private KelolaUserService $kelolaUserService;
+    private RoleService $roleService;
+    private ResponseService $responseService;
+
+
+    public function __construct(KelolaUserService $kelolaUserService, ResponseService $responseService, RoleService $roleService)
+    {
+        $this->kelolaUserService = $kelolaUserService;
+        $this->responseService = $responseService;
+        $this->roleService = $roleService;
+    }
+
+
+    public function index()
+    {
+        $in_kelola_user =  $this->kelolaUserService->mendapatkanSeluruhDataPaginate($this->paginate);
+        return compact("in_kelola_user");
+    }
+
+    public function search(Request $request)
+    {
+        $in_kelola_user  = $this->kelolaUserService->mencariDataBerdasarkanKostum("nama", $request->cari, $this->paginate);
+        return compact("in_kelola_user");
+    }
+
+    public function store(Request $request)
+    {
+        return $this->responseService->menyimpanData($this->kelolaUserService->menyimpanData($request));
+    }
+
+    public function create()
+    {
+        $in_role  = $this->roleService->mendapatkanSeluruhData();
+        return compact("in_role");
+    }
+
+    public function edit($id)
+    {
+        $in_role  = $this->roleService->mendapatkanSeluruhData();
+        $in_kelola_user  = $this->kelolaUserService->mendapatkanSatuData($id);
+        return compact("in_kelola_user", "in_role");
+    }
+
+    public function show($id)
+    {
+        $in_kelola_user = $this->kelolaUserService->mendapatkanSatuData($id);
+        return compact("in_kelola_user");
+    }
+
+    public function update(Request $request, $id)
+    {
+        return $this->responseService->updateData($this->kelolaUserService->memperbaruiData($request, $id));
+    }
+
+    public function destroy($id)
+    {
+        $this->kelolaUserService->menghapusData($id);
+        return $this->responseService->menghapusData($id);
+    }
 }
---
+++ app/Services/KelolaUserService.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class KelolaUserService extends Controller
+{
+
+    private function EloquentDataAktif()
+    {
+        return User::with("getRole")->orderBy('created_at', 'DESC');
+    }
+
+    private function EloquentData()
+    {
+        return User::orderBy('created_at', 'DESC');
+    }
+
+    private function EloquentDataNonaktif()
+    {
+        return User::orderBy('created_at', 'DESC');
+    }
+
+    public function mendapatkanSeluruhData()
+    {
+        return $this->EloquentDataAktif()->get();
+    }
+
+    public function mendapatkanSeluruhDataPaginate($paginate)
+    {
+        return $this->EloquentDataAktif()->paginate($paginate);
+    }
+
+    public function mencariDataBerdasarkanKostum($nama_kolom, $request, $paginate)
+    {
+        return $this->EloquentDataAktif()->where($nama_kolom, "LIKE", '%' . $request . '%')->paginate($paginate);
+    }
+
+    public function mendapatkanSatuData($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    public function menyimpanData($request)
+    {
+        $model = new User();
+        $model->kd_user = $this->get_primaryKey(101);
+        return $this->mengelolaData($model, $request);
+    }
+
+    public function memperbaruiData($request, $id)
+    {
+        $model = User::findOrFail($id);
+        return $this->mengelolaData($model, $request);
+    }
+
+    private function mengelolaData($model, $request)
+    {
+
+        if ($request->nama) {
+            $model->nama = $request->nama;
+        }
+        if ($request->email) {
+            $model->email = $request->email;
+        }
+        if ($request->username) {
+            $model->username = $request->username;
+        }
+        if ($request->role) {
+            $model->role = $request->role;
+        }
+        if ($request->password) {
+            $model->password =  Hash::make($request->password);
+        }
+
+        $model->save();
+
+        return $model;
+    }
+
+    public function menghapusData($id)
+    {
+        $data = User::findOrFail($id)->delete();
+        return $data;
+    }
+}
