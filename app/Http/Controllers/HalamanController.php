0055c302dd81ab7deb8f2ecfd8dceea93ea0e458
amrdevac <amrullahdev@yahoo.com>
2021-11-02 16:17:45 +0700

    menambhkan class getRolePermission untuk mendapatkan data akses aplikasi

--- app/Http/Controllers/HalamanController.php
+++ app/Http/Controllers/HalamanController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\getRolePermission;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HalamanController extends Controller
 {
+
+    private getRolePermission $getRolePermission;
+
+    public function __construct(getRolePermission $getRolePermission)
+    {
+        $this->getRolePermission = $getRolePermission;
+    }
+
     public function __invoke()
     {
         $in_permission = [];
-        return view("layouts.app", compact("in_permission"));
+        $in_sidebars = [];
+        if (Auth::check()) {
+            $permission = $this->getRolePermission->mendapatkanAksesAplikasi();
+            $in_sidebars = $permission[0];
+            $in_permission = $permission[1];
+        }
+
+        return view("layouts.app", compact("in_permission", "in_sidebars"));
     }
 }
