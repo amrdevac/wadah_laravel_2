270c96d69dcc854567919607ecdc2bef47889c64
amrdevac <amrullahdev@yahoo.com>
2021-11-02 16:21:09 +0700

    fixing service called name on subsidebar controller

--- app/Http/Controllers/API/SubSidebarController.php
+++ app/Http/Controllers/API/SubSidebarController.php
@@ -3,9 +3,69 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Services\PermissionService;
+use App\Services\ResponseService;
+use App\Services\SubsidebarService;
 use Illuminate\Http\Request;
 
 class SubSidebarController extends Controller
 {
-    //
+
+    private SubsidebarService $subsidebarService;
+    private ResponseService $responseService;
+    private PermissionService $permissionService;
+
+    public function __construct(
+        SubsidebarService $subsidebarService,
+        ResponseService $responseService,
+        PermissionService $permissionService
+    ) {
+        $this->subsidebarService = $subsidebarService;
+        $this->responseService = $responseService;
+        $this->permissionService = $permissionService;
+    }
+
+    public function index()
+    {
+        $in_subsidebar = $this->subsidebarService->mendapatkanSeluruhData();
+        return compact("in_subsidebar");
+    }
+
+
+    public function show($id)
+    {
+        $in_subsidebar = $this->subsidebarService->mendapatkanSatuData($id);
+        return compact("in_subsidebar");
+    }
+
+    public function create()
+    {
+        $in_permission = $this->permissionService->mendapatkanSeluruhData();
+        return compact('in_permission');
+    }
+
+    public function store(Request $request)
+    {
+        return $this->responseService->menyimpanData(
+            $this->subsidebarService->menyimpanData($request)
+        );
+    }
+    public function edit($id)
+    {
+        $in_subsidebar = $this->subsidebarService->mendapatkanSatuData($id);
+        return compact("in_subsidebar");
+    }
+
+    public function update(Request $request, $id)
+    {
+        return $this->responseService->updateData(
+            $this->subsidebarService->memperbaruiData($request, $id)
+        );
+    }
+
+    public function destroy($id)
+    {
+        $this->subsidebarService->menghapusData($id);
+        return $this->responseService->menghapusData($id);
+    }
 }
