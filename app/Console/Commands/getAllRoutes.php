d94a3ac1d56130f724c40b7a6058de70929c33ec
amrdevac <amrullahdev@yahoo.com>
2021-11-03 20:21:46 +0700

    fixing name

--- app/Console/Commands/getAllRoutes.php
+++ app/Console/Commands/getAllRoutes.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use App\Actions\getAllRoutes as dataApiRoutes;
 use Illuminate\Console\Command;
 
-class getAllRoutes extends Command
+class GetAllRoutes extends Command
 {
     /**
      * The name and signature of the console command.
