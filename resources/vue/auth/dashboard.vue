ec09e39907af1a9cf343a729f6951d758d1a019d
amrdevac <amrullahdev@yahoo.com>
2021-11-03 15:21:17 +0700

    menghilangkan load pada dashboard

--- resources/vue/auth/dashboard.vue
+++ resources/vue/auth/dashboard.vue
@@ -11,23 +11,7 @@ export default {
          in_data: {}
       };
    },
-   mounted() {
-      this.load();
-   },
-   methods: {
-      load() {
-         this.$Progress.start();
-         axios
-            .get("/api/role")
-            .then(respon => {
-               this.in_data = respon.data.in_data;
-               this.$Progress.finish();
-            })
-            .catch(e => {
-               this.$Progress.fail();
-               this.$error.catch(e);
-            });
-      }
-   }
+   mounted() {},
+   methods: {}
 };
 </script>
