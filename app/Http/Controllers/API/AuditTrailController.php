4495bf02bda7cbe0d40eefa589a19ac5869d86a1
amrdevac <amrullahdev@yahoo.com>
2021-11-04 09:51:40 +0700

    create audit trail feature

--- app/Http/Controllers/API/AuditTrailController.php
+++ app/Http/Controllers/API/AuditTrailController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Services\AuditTrailService;
 use Illuminate\Http\Request;
 
 class AuditTrailController extends Controller
 {
-    //
+
+    private AuditTrailService $auditTrailService;
+    public function __construct(AuditTrailService $auditTrailService)
+    {
+        $this->auditTrailService =  $auditTrailService;
+    }
+
+
+    public function index()
+    {
+        $in_audit_trail = $this->auditTrailService->mendapatkanSeluruhDataPaginate($this->paginate);
+        return compact("in_audit_trail");
+    }
+
+    public function show($id)
+    {
+        $in_audit_trail = $this->auditTrailService->mendapatkanSatuData($id);
+        return compact("in_audit_trail");
+    }
 }
