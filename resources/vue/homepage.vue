a0579f4d6570b288de799343961d15c28848786d
amrdevac <amrullahdev@yahoo.com>
2021-11-04 15:10:19 +0700

    adding homepage  as a first opener

---
+++ resources/vue/homepage.vue
@@ -0,0 +1,19 @@
+<template>
+   <login></login>
+</template>
+
+<script>
+import login from "./login.vue";
+
+export default {
+   components: {
+      login
+   },
+   data() {
+      return {};
+   },
+   name: "homepage",
+   mounted() {},
+   methods: {}
+};
+</script>
