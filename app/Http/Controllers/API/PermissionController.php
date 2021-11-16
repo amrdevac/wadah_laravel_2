e9aca571161668abbb6439efc8475bfa34bd7c81
amrdevac <amrullahdev@yahoo.com>
2021-11-16 12:32:11 +0700

    menambahkan destroy pada controller

--- app/Http/Controllers/API/PermissionController.php
+++ app/Http/Controllers/API/PermissionController.php
@@ -4,17 +4,20 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Services\PermissionService;
+use App\Services\ResponseService;
 use Illuminate\Http\Request;
 
 class PermissionController extends Controller
 {
     private PermissionService $permissionService;
+    private ResponseService $responseService;
 
-    public function __construct(PermissionService $permissionService)
+    public function __construct(PermissionService $permissionService, ResponseService $responseService)
     {
         $this->permissionService = $permissionService;
+        $this->responseService = $responseService;
     }
-
+    
     public function index()
     {
         $in_permission = $this->permissionService->mendapatkanSeluruhData();
@@ -25,5 +28,11 @@ class PermissionController extends Controller
     {
         $in_permission = $this->permissionService->mencariDataBerdasarkanKostum("nama_route", $request->cari);
         return compact("in_permission");
+    }
+
+    public function destroy($id)
+    {
+        $this->permissionService->menghapusData($id);
+        return $this->responseService->menghapusData($id);
     }
 }
